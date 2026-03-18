e747ca23db04ee1e5b9c31b0a32d4c29215d8a30
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-03-19 00:03:36 +0100

    better format

--- lez_03/main.c
+++ lez_03/main.c
@@ -9,19 +9,22 @@ void redraw(void) {
   glBegin(GL_QUADS);
 
     glColor3f(1.0, 1.0, 1.0);
-      glVertex2f(0.8, 0.2);
+    glVertex2f(0.8, 0.2);
+
     glColor3f(1.0, 0.0, 0.0);
-      glVertex2f(0.7, -0.5);
-    glColor3f(0.0, 1.0, 0.0);
-      glVertex2f(-0.1, -0.5);
-      glVertex2f(0.5, 0.2);
+    glVertex2f(0.7, -0.5);
     
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex2f(-0.1, -0.5);
+    glVertex2f(0.5, 0.2);
+
     glColor3f(1.0, 1.0, 0.0);
-      glVertex2f(0.6, 0.8);
-      glVertex2f(-0.5, -0.6);
+    glVertex2f(0.6, 0.8);
+    glVertex2f(-0.5, -0.6);
+    
     glColor3f(0.0, 0.0, 1.0);
-      glVertex2f(-0.7, 0.0);
-      glVertex2f(-0.5, 0.6);
+    glVertex2f(-0.7, 0.0);
+    glVertex2f(-0.5, 0.6);
 
   glEnd();
   glFlush();
