a0f208350f87d639a5e84490df7ea4d0b229b74c
gyus-e <giuseppe.amato8@studenti.unina.it>
2026-03-18 09:55:54 +0100

    better configuration for drawing a point

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
+#include <GL/freeglut_std.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
 
 void redraw(void) {
+  glPointSize(3.2);
+  glClearColor(0.0, 0.3, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
   glBegin(GL_POINTS);
-  glVertex2f(0.0, 0.0);
+
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex2f(0.0, 0.0);
+
   glEnd();
   glFlush();
 }
 
-int main (int argc, char **argv){
+int main(int argc, char **argv) {
   glutInit(&argc, argv);
-  glutCreateWindow("Window Title");
+  glutInitDisplayMode(GLUT_RGB);
+  glutInitWindowPosition(300, 100);
+  glutInitWindowSize(150, 150);
+  int window = glutCreateWindow(argv[0]);
   glutDisplayFunc(redraw);
   glutMainLoop();
   return 0;
