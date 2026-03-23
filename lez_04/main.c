2796a68f61c2bd7647ad14487fb65c6ee5692c97
gyus-e <giuseppe.amato8@studenti.unina.it>
2026-03-23 11:18:56 +0100

    pressing space changes bg color

--- lez_04/main.c
+++ lez_04/main.c
@@ -2,11 +2,12 @@
 #include <GL/freeglut_std.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 void redraw(void) {
   glPointSize(3.2);
-  glClearColor(0.0, 0.0, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
   glBegin(GL_QUADS);
 
@@ -34,13 +35,31 @@ void redraw(void) {
 }
 
 
+void keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+    case ' ': // SPACE key
+      glClearColor((float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX, (float)rand() / (float)RAND_MAX, 1.0);
+      glutPostRedisplay();
+      break;
+    case 27: // ESC key
+      exit(0);
+  }
+}
+
 int main(int argc, char **argv) {
+  int window;
+  srand(time(NULL));
+  
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGB);
   glutInitWindowPosition(300, 100);
   glutInitWindowSize(520, 520);
-  int window = glutCreateWindow(argv[0]);
+
+  window = glutCreateWindow(argv[0]);
+  glClearColor(0.0, 0.0, 0.0, 1.0);
   glutDisplayFunc(redraw);
+  glutKeyboardFunc(keyboard);
   glutMainLoop();
+  
   return 0;
 }
