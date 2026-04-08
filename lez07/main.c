b407c21dfec62ba941df9193654c66938d220476
gyus-e <giuseppe.amato8@studenti.unina.it>
2026-04-08 10:03:08 +0200

    chronometer

--- lez07/main.c
+++ lez07/main.c
@@ -8,14 +8,24 @@
 #include <stdlib.h>
 
 /**
-Modellare un orologio 2D
+1: Modellare un orologio 2D
 La lancetta dell'ora avanza a ogni giro completo della lancetta dei minuti
 La lancetta dei minuti avanza con il trascinamento del mouse quando il tasto sx
 è premuto
+
+2: Cronometro 3D
+Tasti START, STOP, RESET
+START fa partire il cronometro
+STOP lo ferma
+RESET lo azzera
+lancetta dei secondi avanza ogni secondo
+lancetta minuti avanza ogni qualvolta la lancetta dei secondi completa un giro
 */
 
+GLdouble secondsRotation = 0.0;
 GLdouble minutesRotation = 0.0;
 GLdouble hoursRotation = 0.0;
+int pause = 0;
 
 void display() {
   const int sides = 360;
@@ -31,6 +41,14 @@ void display() {
 
   drawInscribedPolygon(&center, radius, sides, rgb);
   glColor3d(0.0, 0.0, 0.0);
+
+  glPushMatrix();
+  glRotatef(secondsRotation, 0.0, 0.0, 1.0);
+  glBegin(GL_LINES);
+    glVertex3d(center.x, center.y, center.z);
+    glVertex3d(0.0, 0.75, 0.0);
+  glEnd();
+  glPopMatrix();
 
   glPushMatrix();
   glRotatef(minutesRotation, 0.0, 0.0, 1.0);
@@ -53,24 +71,65 @@ void display() {
 }
 
 GLvoid mouse(GLint button, GLint state, GLint x, GLint y) {
+  const int hour = 360.0/12.0;
+  const int minute = 360.0/60.0;
+  int minutes_per_click = 10;
+  
   switch (button) {
   case GLUT_LEFT_BUTTON:
     if (state == GLUT_DOWN) {
-      minutesRotation -= 45.0;
+      minutesRotation -= minutes_per_click*minute;
       if ((int) minutesRotation % 360 == 0) {
-        hoursRotation -= 30.0;
+        hoursRotation -= hour;
       }
       glutPostRedisplay();
     }
     break;
   case GLUT_RIGHT_BUTTON:
     if (state == GLUT_DOWN) {
-      minutesRotation += 45.0;
+      minutesRotation += minutes_per_click*minute;
       if ((int) minutesRotation % 360 == 0) {
-        hoursRotation += 30.0;
+        hoursRotation += hour;
       }
       glutPostRedisplay();
     }
+    break;
+  }
+}
+
+GLvoid timer(GLint value) {
+  const int second = 360.0/60.0;
+  const int minute = 360.0/60.0;
+  const int hour = 360.0/12.0;
+
+  if (pause) {
+    return;
+  }
+  secondsRotation -= second;
+  if ((int) secondsRotation % 360 == 0) {
+    minutesRotation += minute;
+    if ((int) minutesRotation % 360 == 0) {
+      hoursRotation += hour;
+    }
+  }
+  glutPostRedisplay();
+  glutTimerFunc(1000, timer, 0);
+}
+
+GLvoid keyboard(unsigned char key, int x, int y) {
+  switch (key) {
+  case 's':
+    pause = 0;
+    glutTimerFunc(1000, timer, 0);
+    break;
+  case 'p':
+    pause = 1;
+    break;
+  case 'r':
+    secondsRotation = 0.0;
+    minutesRotation = 0.0;
+    hoursRotation = 0.0;
+    glutPostRedisplay();
     break;
   }
 }
@@ -82,7 +141,9 @@ int main(int argc, char **argv) {
   glutInitWindowSize(500, 500);
   int window = glutCreateWindow("Orologio 2D");
   glutDisplayFunc(display);
-  glutMouseFunc(mouse);
+//   glutMouseFunc(mouse);
+//   glutTimerFunc(1000, timer, 0);
+  glutKeyboardFunc(keyboard);
   glutMainLoop();
   return 0;
 }
