4bbb86d201cefb20d2029947f71095c39107987d
gyus-e <giuseppe.amato8@studenti.unina.it>
2026-04-15 11:28:16 +0200

    figured out normals and lighting

--- lez08/main.c
+++ lez08/main.c
@@ -1,96 +1,48 @@
+#include "utils.h"
 #include <GL/freeglut_std.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <stdio.h>
 
 /**
 Poliedro - casa
 */
-
-typedef struct {
-  double x, y, z;
-} Point3D;
-
-typedef struct  {
-  Point3D v1, v2, v3, v4;
-} Quad;
-
-typedef struct {
-  Point3D v1, v2, v3, v4, v5;
-} Pentagon;
-
-typedef struct {
-    const unsigned int numVerts;
-    const unsigned int numFaces;
-    Point3D *verts;
-    Point3D *norms;
-} Mesh;
-
-Point3D eyePosition = {0.0, 0.0, -5.0};
-Point3D lookAtPoint = {0.0, 0.0, 0.0};
-Point3D upVector = {0.0, 1.0, 0.0};
-
 #define L 0.5
+
+Point eyePosition = {0.0, 0.0, -2.0};
+Point lookAtPoint = {0.0, 0.0, 0.0};
+Point upVector = {0.0, 1.0, 0.0};
+
 const unsigned int numFaces = 6;
 const unsigned int numVerts = 8;
-Point3D verts[8] = {
-    // Lato sinistro
-    {-L, -L, -L},
+Point verts[8] = {
+  // Front face
     {-L, -L, L},
-    {-L, L, -L},
     {-L, L, L}, 
-    // Lato destro
-    {L, -L, -L},
+    {L, L, L},
     {L, -L, L},
+
+  // Back face
+    {-L, -L, -L},
+    {-L, L, -L},
     {L, L, -L},
-    {L, L, L}
+    {L, -L, -L}
 };
 
-// Bottom y=-L: 0, 1, 4, 5
-// Top y=L: 2, 3, 6, 7
-// Sx x=-L: 0, 1, 2, 3
-// Dx x=L: 4, 5, 6, 7
-// Front z=L: 1, 3, 5, 7
-// Back z=-L: 0, 2, 4, 6
-
-Point3D newell(Point3D *verts, unsigned int numVerts) {
-    Point3D normal = {0.0, 0.0, 0.0};
-    for (unsigned int i = 0; i < numVerts; i++) {
-        Point3D p = verts[i];
-        Point3D next = verts[(i + 1) % numVerts];
-        normal.x += (p.y - next.y) * (p.z + next.z);
-        normal.y += (p.z - next.z) * (p.x + next.x);
-        normal.z += (p.x - next.x) * (p.y + next.y);
-    }
-    return normal;
-}
-
-Point3D computeNormal(Mesh *mesh) {
-    Point3D normal = {0.0, 0.0, 0.0};
-    for (unsigned int face = 0; face < mesh->numFaces; face++) {
-        // 0: Sx, 1: Dx, 2: Front, 3: Back, 4: Bottom, 5: Top
-        Point3D faceVerts[4];
-        for (unsigned int j = 0; j < 4; j++) {
-            faceVerts[j] = mesh->verts[(face * 4 + j) % mesh->numVerts];
-    }
-        Point3D faceNormal = newell(faceVerts, 4);
-        normal.x += faceNormal.x;
-        normal.y += faceNormal.y;
-        normal.z += faceNormal.z;
-    }
-    return normal;
-}
 
 
 void reshape(int width, int height) {
-  if (height == 0) height = 1; // Prevent division by zero
+  if (height == 0) height = 1;
   glViewport(0, 0, width, height);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(60.0, (double)width / (double)height, 0.1, 100.0);
+  gluPerspective(60.0, (double)width / (double)height, 0.1, 50.0);
 
   glMatrixMode(GL_MODELVIEW);
 }
+
+
 
 void display() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -103,32 +55,48 @@ void display() {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   
-//   gluLookAt(eyePosition.x, eyePosition.y, eyePosition.z,
-//             lookAtPoint.x, lookAtPoint.y, lookAtPoint.z,
-//             upVector.x, upVector.y, upVector.z);
+  glShadeModel(GL_FLAT);
+
+  gluLookAt(eyePosition.x, eyePosition.y, eyePosition.z,
+            lookAtPoint.x, lookAtPoint.y, lookAtPoint.z,
+            upVector.x, upVector.y, upVector.z);
 
 
   glPushMatrix();
+  glColor3d(1.0, 0.0, 0.0);
   glBegin(GL_QUADS);
-    glColor3d(1.0, 0.0, 0.0);
+    Point frontFaceNormal = newell(verts, 4);
+    glNormal3d(frontFaceNormal.x, frontFaceNormal.y, frontFaceNormal.z);
+    for (int i = 0; i < 4; i++) {
+      glVertex3d(verts[i].x, verts[i].y, verts[i].z);
+    }
     
+    Point backFaceNormal = newell(verts + 4, 4);
+    glNormal3d(backFaceNormal.x, backFaceNormal.y, backFaceNormal.z);
+    for (int i = 4; i < 8; i++) {
+      glVertex3d(verts[i].x, verts[i].y, verts[i].z);
+    }
   glEnd();
   glPopMatrix();
 
   glFlush();
 }
 
+
+
 GLvoid keyboard(unsigned char key, int x, int y) {
   
 }
+
+
 
 int main(int argc, char **argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
   glutInitWindowPosition(0, 0);
   glutInitWindowSize(500, 500);
-  int window = glutCreateWindow("Poliedro - Casa");
-//   glutReshapeFunc(reshape);
+  (void)glutCreateWindow("Poliedro - Casa");
+  glutReshapeFunc(reshape);
   glutDisplayFunc(display);
   glutKeyboardFunc(keyboard);
   glutMainLoop();
---
+++ lez08/utils.c
@@ -0,0 +1,158 @@
+#include "utils.h"
+#include <GL/freeglut_std.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
+
+void checkErrors(const char *label) {
+  GLenum err;
+  while ((err = glGetError()) != GL_NO_ERROR) {
+    fprintf(stderr, "%s: %s\n", label, gluErrorString(err));
+  }
+}
+
+Point getPointOnCircumference(const Point *center, const double radius,
+                              const double angle) {
+  Point p;
+  p.x = center->x + radius * cos(angle);
+  p.y = center->y + radius * sin(angle);
+  p.z = center->z;
+  return p;
+}
+
+int drawInscribedPolygon(const Point *center, const double radius,
+                         const int sides, const double rgb[3]) {
+  double angle, x, y, z;
+  int i;
+  if (sides < 3) {
+    return -1;
+  }
+  glColor3dv(rgb);
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex3f(center->x, center->y, center->z);
+
+  for (i = 0; i <= sides; i++) {
+    angle = 2 * M_PI * i / sides;
+    Point p = getPointOnCircumference(center, radius, angle);
+    glVertex3f(p.x, p.y, p.z);
+  }
+  glEnd();
+  return GL_NO_ERROR;
+}
+
+void drawRectangle(const Point points[4], const double rgb[3]) {
+  int i;
+  glColor3dv(rgb);
+  glBegin(GL_QUADS);
+  for (i = 0; i < 4; i++) {
+    glVertex3f(points[i].x, points[i].y, points[i].z);
+  }
+  glEnd();
+}
+
+int drawStar(const Point *center, const int sides, const double r1, const double r2, const double rgb[3]) {
+  const double radius[2] = {r1, r2};
+  double angle;
+  int i;
+  if (sides < 3) {
+    return -1;
+  }
+  glColor3dv(rgb);
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex3d(center->x, center->y, center->z);
+
+  for (i = 0; i <= 2*sides; i++) {
+    angle = 2 * M_PI * i / sides;
+    Point p = getPointOnCircumference(center, radius[i % 2], angle);
+    glVertex3d(p.x, p.y, p.z);
+  }
+  glEnd();
+  return GL_NO_ERROR;
+}
+
+void drawPrism(const GLint sides,
+              const Point *lower_base_center,
+              const Point *upper_base_center,
+              const GLdouble radius,
+              const GLdouble rgb[3]) {
+  Point points[4];
+  GLdouble angle, x, y, z;
+  int i;
+
+  drawInscribedPolygon(lower_base_center, radius, sides, rgb);
+  drawInscribedPolygon(upper_base_center, radius, sides, rgb);
+  for (i = 0; i <= sides; i++) {
+    angle = 2 * M_PI * i / sides;
+    points[0] = getPointOnCircumference(lower_base_center, radius, angle);
+    points[1] = getPointOnCircumference(upper_base_center, radius, angle);
+
+    angle = 2 * M_PI * (i + 1) / sides;
+    points[2] = getPointOnCircumference(upper_base_center, radius, angle);
+    points[3] = getPointOnCircumference(lower_base_center, radius, angle);
+
+    drawRectangle(points, rgb);
+  }
+}
+
+void draw2DClock(const Point *center, const double radius, const double secondsRotation, const double minutesRotation, const double hoursRotation, const double bg[3], const double fg[3]) {
+  const int sides = 360;
+
+  drawInscribedPolygon(center, radius, sides, bg);
+  
+  glColor3dv(fg);
+  for (int i = 12; i > 0 ; --i) {
+    double angle = 2*M_PI - i*hour*2*M_PI/360.0 + M_PI/2.0;
+    double x = (radius-0.1)*cos(angle);
+    double y = (radius-0.1)*sin(angle);
+    glRasterPos3d(x, y, 0.0);
+    
+    char number[3];
+    sprintf(number, "%d", i);
+    for (char *c = number; *c != '\0'; ++c) {
+      glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *c);
+    }
+  }
+  
+  glPushMatrix();
+  glRotated(secondsRotation, 0.0, 0.0, 1.0);
+  glBegin(GL_LINES);
+    glVertex3d(center->x, center->y, center->z);
+    glVertex3d(0.0, 0.45, 0.0);
+  glEnd();
+  glPopMatrix();
+
+  glPushMatrix();
+  glRotated(minutesRotation, 0.0, 0.0, 1.0);
+  glBegin(GL_LINES);
+    glVertex3d(center->x, center->y, center->z);
+    glVertex3d(0.0, 0.4, 0.0);
+  glEnd();
+  glPopMatrix();
+
+  glPushMatrix();
+  glRotated(hoursRotation, 0.0, 0.0, 1.0);
+  glBegin(GL_LINES);
+    glVertex3d(center->x, center->y, center->z);
+    glVertex3d(0.25, 0.0, 0.0);
+  glEnd();
+  glPopMatrix();
+}
+
+void draw3DClock(const Point *center, const double radius, const double secondsRotation, const double minutesRotation, const double hoursRotation, const double bg[3], const double fg[3]) {
+  Point backCenter = {center->x, center->y, center->z - 0.5};
+  draw2DClock(center, radius, secondsRotation, minutesRotation, hoursRotation, bg, fg);
+  draw2DClock(&backCenter, radius, secondsRotation, minutesRotation, hoursRotation, bg, fg);
+}
+
+Point newell(Point *verts, unsigned int numVerts) {
+    Point normal = {0.0, 0.0, 0.0};
+    for (unsigned int i = 0; i < numVerts; i++) {
+        Point p = verts[i];
+        Point next = verts[(i + 1) % numVerts];
+        normal.x += (p.y - next.y) * (p.z + next.z);
+        normal.y += (p.z - next.z) * (p.x + next.x);
+        normal.z += (p.x - next.x) * (p.y + next.y);
+    }
+    return normal;
+}
