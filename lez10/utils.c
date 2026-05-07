5ffc5aadfba2bf7502b2992ab557a8045322e366
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-05-07 22:09:31 +0200

    fixed utils

--- lez10/utils.c
+++ lez10/utils.c
@@ -28,55 +28,52 @@ Point getPointOnCircumference(const Point *center, const double radius,
 
 
 int drawInscribedPolygon(const Point *center, const double radius,
-                         const int sides, const double rgb[3]) {
+                         const int sides) {
   double angle, x, y, z;
   int i;
   if (sides < 3) {
     return -1;
   }
-  glColor3dv(rgb);
   glBegin(GL_TRIANGLE_FAN);
-  glVertex3f(center->x, center->y, center->z);
+    glVertex3f(center->x, center->y, center->z);
 
-  for (i = 0; i <= sides; i++) {
-    angle = 2 * M_PI * i / sides;
-    Point p = getPointOnCircumference(center, radius, angle);
-    glVertex3f(p.x, p.y, p.z);
-  }
+    for (i = 0; i <= sides; i++) {
+      angle = 2 * M_PI * i / sides;
+      Point p = getPointOnCircumference(center, radius, angle);
+      glVertex3f(p.x, p.y, p.z);
+    }
   glEnd();
   return GL_NO_ERROR;
 }
 
 
 
-void drawRectangle(const Point points[4], const double rgb[3]) {
+void drawRectangle(const Point points[4]) {
   int i;
-  glColor3dv(rgb);
   glBegin(GL_QUADS);
-  for (i = 0; i < 4; i++) {
-    glVertex3f(points[i].x, points[i].y, points[i].z);
-  }
+    for (i = 0; i < 4; i++) {
+      glVertex3f(points[i].x, points[i].y, points[i].z);
+    }
   glEnd();
 }
 
 
 
-int drawStar(const Point *center, const int sides, const double r1, const double r2, const double rgb[3]) {
+int drawStar(const Point *center, const int sides, const double r1, const double r2) {
   const double radius[2] = {r1, r2};
   double angle;
   int i;
   if (sides < 3) {
     return -1;
   }
-  glColor3dv(rgb);
   glBegin(GL_TRIANGLE_FAN);
-  glVertex3d(center->x, center->y, center->z);
+    glVertex3d(center->x, center->y, center->z);
 
-  for (i = 0; i <= 2*sides; i++) {
-    angle = 2 * M_PI * i / sides;
-    Point p = getPointOnCircumference(center, radius[i % 2], angle);
-    glVertex3d(p.x, p.y, p.z);
-  }
+    for (i = 0; i <= 2*sides; i++) {
+      angle = 2 * M_PI * i / sides;
+      Point p = getPointOnCircumference(center, radius[i % 2], angle);
+      glVertex3d(p.x, p.y, p.z);
+    }
   glEnd();
   return GL_NO_ERROR;
 }
@@ -86,14 +83,13 @@ int drawStar(const Point *center, const int sides, const double r1, const double
 void drawPrism(const GLint sides,
               const Point *lower_base_center,
               const Point *upper_base_center,
-              const GLdouble radius,
-              const GLdouble rgb[3]) {
+              const GLdouble radius) {
   Point points[4];
   GLdouble angle, x, y, z;
   int i;
 
-  drawInscribedPolygon(lower_base_center, radius, sides, rgb);
-  drawInscribedPolygon(upper_base_center, radius, sides, rgb);
+  drawInscribedPolygon(lower_base_center, radius, sides);
+  drawInscribedPolygon(upper_base_center, radius, sides);
   for (i = 0; i <= sides; i++) {
     angle = 2 * M_PI * i / sides;
     points[0] = getPointOnCircumference(lower_base_center, radius, angle);
@@ -103,7 +99,7 @@ void drawPrism(const GLint sides,
     points[2] = getPointOnCircumference(upper_base_center, radius, angle);
     points[3] = getPointOnCircumference(lower_base_center, radius, angle);
 
-    drawRectangle(points, rgb);
+    drawRectangle(points);
   }
 }
 
@@ -112,7 +108,8 @@ void drawPrism(const GLint sides,
 void draw2DClock(const Point *center, const double radius, const double secondsRotation, const double minutesRotation, const double hoursRotation, const double bg[3], const double fg[3]) {
   const int sides = 360;
 
-  drawInscribedPolygon(center, radius, sides, bg);
+  glColor3dv(bg);
+  drawInscribedPolygon(center, radius, sides);
   
   glColor3dv(fg);
   for (int i = 12; i > 0 ; --i) {
@@ -129,27 +126,27 @@ void draw2DClock(const Point *center, const double radius, const double secondsR
   }
   
   glPushMatrix();
-  glRotated(secondsRotation, 0.0, 0.0, 1.0);
-  glBegin(GL_LINES);
-    glVertex3d(center->x, center->y, center->z);
-    glVertex3d(0.0, 0.45, 0.0);
-  glEnd();
+    glRotated(secondsRotation, 0.0, 0.0, 1.0);
+    glBegin(GL_LINES);
+      glVertex3d(center->x, center->y, center->z);
+      glVertex3d(0.0, 0.45, 0.0);
+    glEnd();
   glPopMatrix();
 
   glPushMatrix();
-  glRotated(minutesRotation, 0.0, 0.0, 1.0);
-  glBegin(GL_LINES);
-    glVertex3d(center->x, center->y, center->z);
-    glVertex3d(0.0, 0.4, 0.0);
-  glEnd();
+    glRotated(minutesRotation, 0.0, 0.0, 1.0);
+    glBegin(GL_LINES);
+      glVertex3d(center->x, center->y, center->z);
+      glVertex3d(0.0, 0.4, 0.0);
+    glEnd();
   glPopMatrix();
 
   glPushMatrix();
-  glRotated(hoursRotation, 0.0, 0.0, 1.0);
-  glBegin(GL_LINES);
-    glVertex3d(center->x, center->y, center->z);
-    glVertex3d(0.25, 0.0, 0.0);
-  glEnd();
+    glRotated(hoursRotation, 0.0, 0.0, 1.0);
+    glBegin(GL_LINES);
+      glVertex3d(center->x, center->y, center->z);
+      glVertex3d(0.25, 0.0, 0.0);
+    glEnd();
   glPopMatrix();
 }
 
--- lez10/utils.h
+++ lez10/utils.h
@@ -28,12 +28,13 @@ static const double white[] = {1.0, 1.0, 1.0};
 static const double black[] = {0.0, 0.0, 0.0};
 static const double blue[] = {0.0, 0.0, 1.0};
 static const double red[] = {1.0, 0.0, 0.0};
+static const double green[] = {0.0, 1.0, 0.0};
 
 void checkErrors(const char *label);
 Point getPointOnCircumference(const Point *center, const double radius, const double angle);
-int drawInscribedPolygon(const Point *center, const double radius, const int sides, const double rgb[3]);
-void drawRectangle(const Point points[4], const double rgb[3]);
-void drawPrism(const int sides, const Point *lower_base_center, const Point *upper_base_center, const double radius, const double rgb[3]);
+int drawInscribedPolygon(const Point *center, const double radius, const int sides);
+void drawRectangle(const Point points[4]);
+void drawPrism(const int sides, const Point *lower_base_center, const Point *upper_base_center, const double radius);
 void draw2DClock(const Point *center, const double radius, const double secondsRotation, const double minutesRotation, const double hoursRotation, const double bg[3], const double fg[3]);
 void draw3DClock(const Point *center, const double radius, const double secondsRotation, const double minutesRotation, const double hoursRotation, const double bg[3], const double fg[3]);
 void drawPolyedron(const Mesh *mesh);
