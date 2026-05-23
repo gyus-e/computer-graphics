d3ea57a2c00f744bd6d2aaa660e5445d0365f1b7
gyus-e <giuseppe.amato8@studenti.unina.it>
2026-05-23 16:35:21 +0200

    fixing lez11

--- lez11/main.c
+++ lez11/main.c
@@ -6,31 +6,33 @@
 
 
 
-GLUnurbsObj *theNurb;
+GLUnurbsObj *theNurb, *theNurb2, *theNurb3;
 
 
-float cp[4][3] = {
-    {-4.0, -4.0, 0.0},
-    {-2.0, 4.0, 0.0},
-    {2.0, -4.0, 0.0},
-    {4.0, 4.0, 0.0}
+GLfloat cp[4 * 3] = {
+    -4.0, -4.0, 0.0,
+    -2.0, 4.0, 0.0,
+    2.0, -4.0, 0.0,
+    4.0, 4.0, 0.0
 };
+
 float w[4] = {1.0, 5.0, 1.0, 1.0};
 float knots[8] = {0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0};
-float cpw[4][4];
 
-float circleCP[7][3] = {
-    {1.0, 2.0, 0.0},
-    {1.0, 3.0, 0.0},
-    {3.0, 3.0, 0.0},
-    {3.0, 2.0, 0.0},
-    {3.0, 1.0, 0.0},
-    {1.0, 1.0, 0.0},
-    {1.0, 2.0, 0.0}
+GLfloat cpw[4 * 4];
+
+GLfloat circleCP[7 * 4] = {
+    1.0, 2.0, 0.0, 0.0,
+    1.0, 3.0, 0.0, 0.0,
+    3.0, 3.0, 0.0, 0.0,
+    3.0, 2.0, 0.0, 0.0,
+    3.0, 1.0, 0.0, 0.0,
+    1.0, 1.0, 0.0, 0.0,
+    1.0, 2.0, 0.0, 0.0
 };
 float circleW[7] = {1.0, 0.5, 0.5, 1.0, 0.5, 0.5, 1.0};
 float circleKnots[10] = {0.0, 0.0, 0.0, 0.25, 0.5, 0.5, 0.75, 1.0, 1.0, 1.0};
-float circleCpw[7][4];
+GLfloat circleCpw[7 * 4];
 
 
 Point camPosition = {0.0, 0.0, 10.0};
@@ -55,53 +57,58 @@ void display() {
   glClearDepth(1.0);
   glEnable(GL_DEPTH_TEST);
   
-  glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0);
-  
-  glPointSize(1.0);
-
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   
-  glShadeModel(GL_FLAT);
-
   glPushMatrix();
-    gluLookAt(camPosition.x, camPosition.y, camPosition.z,
-              lookAtPoint.x, lookAtPoint.y, lookAtPoint.z,
-              upVector.x, upVector.y, upVector.z);
+    gluLookAt(camPosition[X], camPosition[Y], camPosition[Z],
+              lookAtPoint[X], lookAtPoint[Y], lookAtPoint[Z],
+              upVector[X], upVector[Y], upVector[Z]);
 
     glPushMatrix();
-      glColor3dv(red);
+      glPointSize(1.0);
+  
+      glColor3fv(red);
       theNurb = gluNewNurbsRenderer();
       gluNurbsCallback(theNurb, GLU_ERROR, (GLvoid (*))nurbsError);
       gluNurbsProperty(theNurb, GLU_U_STEP, 360);
       gluNurbsProperty(theNurb, GLU_SAMPLING_METHOD, GLU_DOMAIN_DISTANCE);
       gluBeginCurve(theNurb);
-        // gluNurbsCurve(theNurb, 8, knots, 3, &cp[0][0], 4, GL_MAP1_VERTEX_3);
-        // gluNurbsCurve(theNurb, 8, knots, 4, &cpw[0][0], 4, GL_MAP1_VERTEX_4);
-        gluNurbsCurve(theNurb, 10, circleKnots, 4, &circleCpw[0][0], 3, GL_MAP1_VERTEX_4);
+        gluNurbsCurve(theNurb, 8, knots, 3, &cp[0], 4, GL_MAP1_VERTEX_3);
       gluEndCurve(theNurb);
+
+      glColor3fv(green);
+      theNurb2 = gluNewNurbsRenderer();
+      gluNurbsCallback(theNurb2, GLU_ERROR, (GLvoid (*))nurbsError);
+      gluNurbsProperty(theNurb2, GLU_U_STEP, 360);
+      gluNurbsProperty(theNurb2, GLU_SAMPLING_METHOD, GLU_DOMAIN_DISTANCE);
+      gluBeginCurve(theNurb2);
+        gluNurbsCurve(theNurb2, 8, knots, 4, &cpw[0], 4, GL_MAP1_VERTEX_4);
+      gluEndCurve(theNurb2);
+
+      // glColor3fv(blue);
+      // theNurb3 = gluNewNurbsRenderer();
+      // gluNurbsCallback(theNurb3, GLU_ERROR, (GLvoid (*))nurbsError);
+      // gluNurbsProperty(theNurb3, GLU_U_STEP, 360);
+      // gluNurbsProperty(theNurb3, GLU_SAMPLING_METHOD, GLU_DOMAIN_DISTANCE);
+      // gluBeginCurve(theNurb3);
+      //   gluNurbsCurve(theNurb3, 10, circleKnots, 4, (float*)circleCpw[0], 3, GL_MAP1_VERTEX_4);
+      // gluEndCurve(theNurb3);
+  
     glPopMatrix();
   glPopMatrix();
   glutSwapBuffers();
 }
 
-
-void omogenizePoints(double cpw[][4], const Point cp[], const size_t numPoints, const double w[]) {
+void omogenizePoints(Point cpw[], const Point cp[], const size_t numPoints, const float w[]) {
   for (int i = 0; i < numPoints; i++) {
-    (void)omogenize3dp(cpw[i], &cp[i], w[i]);
-  }
-}
-
-void omogenizePoints2(double cpw[][4], const double cp[][3], const size_t numPoints, const double w[]) {
-  for (int i = 0; i < numPoints; i++) {
-    (void)omogenize3dv(cpw[i], cp[i], w[i]);
+    toHomogeneousCoordinates(cpw[i], cp[i], w[i]);
   }
 }
 
 int main(int argc, char **argv) {
-  omogenizePoints2((double**)cpw, (double**)cp, 4, (double*)w);
-  omogenizePoints2((double**)circleCpw, (double**)circleCP, 7, (double*)circleW);
+  omogenizePoints(cpw, cp, 4, w);
+  omogenizePoints(circleCpw, circleCP, 7, circleW);
 
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
