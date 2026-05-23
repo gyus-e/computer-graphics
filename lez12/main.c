f7ae83761f63fd86fa534baf6eb42e35f4cecd4e
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-05-23 19:17:29 +0200

    fixed lez12

--- lez12/main.c
+++ lez12/main.c
@@ -3,29 +3,50 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <math.h>
 
+int displayExercise = 1;
 
-
-Point camPosition = {5.0, 0.0, 20.0};
 const Point lookAtPoint = {0.0, 0.0, 0.0};
 const Point upVector = {0.0, 1.0, 0.0};
 
+const double camDistance = 20.0;
+const double sensitivity = 0.01;
+double camAngle[2] = {0.0, 0.0};
+Point camPosition = {0.0, 0.0, camDistance};
 
+int leftMouseDown = 0;
+int rightMouseDown = 0;
+int lastMouseX = 0;
+int lastMouseY = 0;
 
-const GLdouble cp[4][4][3] = {
-    {{0, 0, 0}, {1, 0, 0.5}, {2, 0, 1}, {3, 0, 0}},
-    {{0, 1, 0}, {1, 1, 3}, {2, 1, 3}, {3, 1, 0}},
-    {{0, 2, 0}, {1, 2, 3}, {2, 2, 3}, {3, 2, 0}},
-    {{0, 3, 0}, {1, 3, 1}, {2, 3, 0.5}, {3, 3, 0}}};
-
-const GLdouble w[4][4] = {
-    {1, .5, .5, 1}, {1, .5, .5, 1}, {1, .5, .5, 1}, {1, .5, .5, 1}};
-
-GLdouble cpw[4][4][4];
 
 const double u1 = 0.0, u2 = 1.0, v1 = 0.0, v2 = 1.0;
 const unsigned int uorder = 4, vorder = 4;
 const unsigned int n = 40, m = 40;
+
+const GLdouble w[4][4] = {
+    {1, 0.5, 0.5, 1}, {1, 0.5, 0.5, 1}, {1, 0.5, 0.5, 1}, {1, 0.5, 0.5, 1}
+};
+
+const GLdouble cp1[4][4][3] = {
+    {{0, 0, 0}, {1, 0, 0.5}, {2, 0, 1}, {3, 0, 0}},
+    {{0, 1, 0}, {1, 1, 3}, {2, 1, 3}, {3, 1, 0}},
+    {{0, 2, 0}, {1, 2, 3}, {2, 2, 3}, {3, 2, 0}},
+    {{0, 3, 0}, {1, 3, 1}, {2, 3, 0.5}, {3, 3, 0}}
+};
+GLdouble cpw1[4][4][4];
+
+const GLfloat cp2[4][4][3] = {
+    {{-3, -3, -3}, {-3, -1, -3}, {-3, 1, -3}, {-3, 3, -3}},
+    {{-1, -3, -3}, {-1, -1, 3}, {-1, 1, 3}, {-1, 3, -3}},
+    {{1, -3, -3}, {1, -1, 3}, {1, 1, 3}, {1, 3, -3}},
+    {{3, -3, -3}, {3, -1, -3}, {3, 1, -3}, {3, 3, -3}}
+};
+GLfloat cpw2[4][4][4];
+GLfloat knots[8] = {0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0};
+GLUnurbsObj *theNurb;
+
 
 
 
@@ -45,70 +66,91 @@ void display() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glClearDepth(1.0);
   glEnable(GL_DEPTH_TEST);
-
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
-
-  glPointSize(1.0);
+  glEnable(GL_AUTO_NORMAL);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-
   glShadeModel(GL_FLAT);
-
+  glPointSize(1.0);
   glPushMatrix();
-  gluLookAt(camPosition.x, camPosition.y, camPosition.z, lookAtPoint.x,
-            lookAtPoint.y, lookAtPoint.z, upVector.x, upVector.y, upVector.z);
 
-  glPushMatrix();
-  glColor3dv(red);
+    gluLookAt(camPosition[X], camPosition[Y], camPosition[Z], lookAtPoint[X],
+              lookAtPoint[Y], lookAtPoint[Z], upVector[X], upVector[Y], upVector[Z]);
 
-  // glMap2d(GL_MAP2_VERTEX_3, u1, u2, 3, uorder, v1, v2, 12, vorder, &cp[0][0][0]);
-  // glEnable(GL_MAP2_VERTEX_3);
-  
-  glMap2d(GL_MAP2_VERTEX_4, u1, u2, 4, uorder, v1, v2, 16, vorder, &cpw[0][0][0]);
-  glEnable(GL_MAP2_VERTEX_4);
+    if (displayExercise == 1) {
+      glPushMatrix();
+        glColor3fv(red);
+        glMap2d(GL_MAP2_VERTEX_3, u1, u2, 3, uorder, v1, v2, 12, vorder, &cp1[0][0][0]);
+        glEnable(GL_MAP2_VERTEX_3);
 
-  glEnable(GL_AUTO_NORMAL);
+        glMap2d(GL_MAP2_VERTEX_4, u1, u2, 4, uorder, v1, v2, 16, vorder, &cpw1[0][0][0]);
+        glEnable(GL_MAP2_VERTEX_4);
 
-  glMapGrid2d(n, u1, u2, m, v1, v2);
-  glEvalMesh2(GL_FILL, 0, n, 0, m);
+        glMapGrid2d(n, u1, u2, m, v1, v2);
+        glEvalMesh2(GL_FILL, 0, n, 0, m);
+      glPopMatrix(); 
+    } else {
+      glPushMatrix();
+        glColor3fv(blue);
+        theNurb = gluNewNurbsRenderer();
+        gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
+        gluBeginSurface(theNurb);
+          // gluNurbsSurface(theNurb, 8, knots, 8, knots, 4*3, 3, &cp2[0][0][0], 4, 4, GL_MAP2_VERTEX_3);
+          gluNurbsSurface(theNurb, 8, knots, 8, knots, 4*4, 4, &cpw2[0][0][0], 4, 4, GL_MAP2_VERTEX_4);
+        gluEndSurface(theNurb);
+      glPopMatrix();
+    }     
 
-  glPopMatrix();
   glPopMatrix();
   glutSwapBuffers();
 }
 
 
-
-void keyboard(unsigned char key, int x, int y) {
+GLvoid keyboard(GLubyte key, GLint x, GLint y) {
   switch (key) {
-  case 27: // ESC
+  case 27:
     exit(0);
     break;
-  case 'w':
-    camPosition.y += 0.5;
-    glutPostRedisplay();
+  case '1':
+    displayExercise = 1;
     break;
-  case 's':
-    camPosition.y -= 0.5;
-    glutPostRedisplay();
-    break;
-  case 'a':
-    camPosition.x -= 0.5;
-    glutPostRedisplay();
-    break;
-  case 'd':
-    camPosition.x += 0.5;
-    glutPostRedisplay();
-    break;
-  case 'r':
-    camPosition = (Point){5.0, 0.0, 20.0};
-    glutPostRedisplay();
-    break;
-  default:
+  case '2':
+    displayExercise = 2;
     break;
   }
+  glutPostRedisplay();
+}
+
+
+GLvoid mouse(GLint button, GLint state, GLint x, GLint y) {
+  switch (button) {
+  case GLUT_LEFT_BUTTON:
+    leftMouseDown = (state == GLUT_DOWN);
+  case GLUT_RIGHT_BUTTON:
+    rightMouseDown = (state == GLUT_DOWN);
+  default:
+    lastMouseX = x;
+    lastMouseY = y;
+  }
+}
+
+
+
+GLvoid motion(GLint x, GLint y) {
+  int dx = x - lastMouseX;
+  int dy = y - lastMouseY;
+  lastMouseX = x;
+  lastMouseY = y;
+  if (leftMouseDown) { 
+    camAngle[Y] += dy * sensitivity;
+    camAngle[X] -= dx * sensitivity;
+  }
+  camPosition[Y] = lookAtPoint[Y] + camDistance * sin(camAngle[Y]);
+  camPosition[X] = lookAtPoint[X] + camDistance * cos(camAngle[Y]) * sin(camAngle[X]);
+  camPosition[Z] = lookAtPoint[Z] + camDistance * cos(camAngle[Y]) * cos(camAngle[X]);
+  glutPostRedisplay();
 }
 
 
@@ -116,10 +158,18 @@ void keyboard(unsigned char key, int x, int y) {
 int main(int argc, char **argv) {
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      cpw[i][j][X] = cp[i][j][X] * w[i][j];
-      cpw[i][j][Y] = cp[i][j][Y] * w[i][j];
-      cpw[i][j][Z] = cp[i][j][Z] * w[i][j];
-      cpw[i][j][3] = w[i][j];
+      cpw1[i][j][X] = cp1[i][j][X] * w[i][j];
+      cpw1[i][j][Y] = cp1[i][j][Y] * w[i][j];
+      cpw1[i][j][Z] = cp1[i][j][Z] * w[i][j];
+      cpw1[i][j][3] = w[i][j];
+    }
+  }
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      cpw2[i][j][X] = cp2[i][j][X] * w[i][j];
+      cpw2[i][j][Y] = cp2[i][j][Y] * w[i][j];
+      cpw2[i][j][Z] = cp2[i][j][Z] * w[i][j];
+      cpw2[i][j][3] = w[i][j];
     }
   }
 
@@ -127,10 +177,12 @@ int main(int argc, char **argv) {
   glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
   glutInitWindowPosition(10, 10);
   glutInitWindowSize(800, 800);
-  (void)glutCreateWindow("BEZIER SURFACES");
+  (void)glutCreateWindow("SURFACES");
   glutReshapeFunc(reshape);
   glutDisplayFunc(display);
   glutKeyboardFunc(keyboard);
+  glutMouseFunc(mouse);
+  glutMotionFunc(motion);
   glutMainLoop();
   return 0;
 }
--- lez12/utils.c
+++ lez12/utils.c
@@ -4,8 +4,13 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
+
+const float white[3] = {1.0, 1.0, 1.0};
+const float black[3] = {0.0, 0.0, 0.0};
+const float red[3] = {1.0, 0.0, 0.0};
+const float green[3] = {0.0, 1.0, 0.0};
+const float blue[3] = {0.0, 0.0, 1.0};
 
 
 void checkErrors(const char *label) {
@@ -15,20 +20,106 @@ void checkErrors(const char *label) {
   }
 }
 
-
-
-Point getPointOnCircumference(const Point *center, const double radius,
-                              const double angle) {
-  Point p;
-  p.x = center->x + radius * cos(angle);
-  p.y = center->y + radius * sin(angle);
-  p.z = center->z;
-  return p;
+void nurbsError(GLenum errorCode) {
+  const GLubyte *errorString = gluErrorString(errorCode);
+  fprintf(stderr, "Errore NURBS: %s\n", errorString);
+  exit(EXIT_FAILURE);
 }
 
 
+void copyPoint(Point dest, const Point src) {
+  dest[X] = src[X];
+  dest[Y] = src[Y];
+  dest[Z] = src[Z];
+}
 
-int drawInscribedPolygon(const Point *center, const double radius,
+
+void toHomogeneousCoordinates(Point dest, const Point p, const double w) {
+  if (dest == NULL || p == NULL) {
+    fprintf(stderr, "Error: dest and p cannot be NULL.\n");
+    return;
+  }
+  if (w == 0.0) {
+    fprintf(stderr, "Error: w cannot be zero.\n");
+    return;
+  }
+  dest[X] = p[X] * w;
+  dest[Y] = p[Y] * w;
+  dest[Z] = p[Z] * w;
+  dest[W] = w;
+}
+
+
+int checkContinuity1(const Point CP1[], const unsigned int N1,
+                    const Point CP2[], const unsigned int N2) {
+  if (CP1[N1-1][X] != CP2[0][X] || 
+      CP1[N1-1][Y] != CP2[0][Y] || 
+      CP1[N1-1][Z] != CP2[0][Z]) {
+    fprintf(stderr, "Error: The last control point of the first curve must be the same as the first control point of the second curve.\n");
+    return 0;
+  }
+  return 1;
+}
+
+
+int checkContinuity2(const Point CP1[], const unsigned int N1,
+                    const Point CP2[], const unsigned int N2) {
+  if (CP1[N1-1][X] - CP1[N1-2][X] != CP2[1][X] - CP2[0][X] || 
+      CP1[N1-1][Y] - CP1[N1-2][Y] != CP2[1][Y] - CP2[0][Y] || 
+      CP1[N1-1][Z] - CP1[N1-2][Z] != CP2[1][Z] - CP2[0][Z]) {
+    fprintf(stderr, "Error: The tangent at the joining point is not the same for both curves.\n");
+    return 0;
+  }
+  return 1;
+}
+
+
+void newell(Point dest, const Point verts[], const unsigned int numVerts) {
+    Point normal = {0.0, 0.0, 0.0};
+    for (unsigned int i = 0; i < numVerts; i++) {
+        Point p, next;
+        copyPoint(p, verts[i]);
+        copyPoint(next, verts[(i + 1) % numVerts]);
+        normal[X] += (p[Y] - next[Y]) * (p[Z] + next[Z]);
+        normal[Y] += (p[Z] - next[Z]) * (p[X] + next[X]);
+        normal[Z] += (p[X] - next[X]) * (p[Y] + next[Y]);
+    }
+    dest[X] = normal[X];
+    dest[Y] = normal[Y];
+    dest[Z] = normal[Z];
+}
+
+
+void casteljau(Point dest, const Point CP[], const unsigned int N, const double t) {
+    Point *p = malloc(N * sizeof(Point));
+    for (int i = 0; i < N; i++) {
+        copyPoint(p[i], CP[i]);
+    }
+    for (int r = 1; r < N; r++) {
+        for (int i = 0; i < N - r; i++) {
+            double x = (1-t)*p[i][X] + t*p[i+1][X];
+            double y = (1-t)*p[i][Y] + t*p[i+1][Y];
+            double z = (1-t)*p[i][Z] + t*p[i+1][Z];
+            double w = (1-t)*p[i][W] + t*p[i+1][W];
+            p[i][X] = x;
+            p[i][Y] = y;
+            p[i][Z] = z;
+            p[i][W] = w;
+        }
+    }
+    copyPoint(dest, p[0]);
+    free(p);
+}
+
+
+void getPointOnCircumference(Point dest, const Point center, const double radius, const double angle) {
+  dest[X] = center[X] + radius * cos(angle);
+  dest[Y] = center[Y] + radius * sin(angle);
+  dest[Z] = center[Z];
+}
+
+
+int drawInscribedPolygon(const Point center, const double radius,
                          const int sides) {
   double angle, x, y, z;
   int i;
@@ -36,293 +127,99 @@ int drawInscribedPolygon(const Point *center, const double radius,
     return -1;
   }
   glBegin(GL_TRIANGLE_FAN);
-    glVertex3f(center->x, center->y, center->z);
-
+    glVertex3f(center[X], center[Y], center[Z]);
     for (i = 0; i <= sides; i++) {
       angle = 2 * M_PI * i / sides;
-      Point p = getPointOnCircumference(center, radius, angle);
-      glVertex3f(p.x, p.y, p.z);
+      Point p;
+      getPointOnCircumference(p, center, radius, angle);
+      glVertex3f(p[X], p[Y], p[Z]);
     }
   glEnd();
   return GL_NO_ERROR;
 }
-
-
-
-void drawRectangle(const Point points[4]) {
-  int i;
-  glBegin(GL_QUADS);
-    for (i = 0; i < 4; i++) {
-      glVertex3f(points[i].x, points[i].y, points[i].z);
-    }
-  glEnd();
-}
-
-
-
-int drawStar(const Point *center, const int sides, const double r1, const double r2) {
-  const double radius[2] = {r1, r2};
-  double angle;
-  int i;
-  if (sides < 3) {
-    return -1;
-  }
-  glBegin(GL_TRIANGLE_FAN);
-    glVertex3d(center->x, center->y, center->z);
-
-    for (i = 0; i <= 2*sides; i++) {
-      angle = 2 * M_PI * i / sides;
-      Point p = getPointOnCircumference(center, radius[i % 2], angle);
-      glVertex3d(p.x, p.y, p.z);
-    }
-  glEnd();
-  return GL_NO_ERROR;
-}
-
-
-
-void drawPrism(const GLint sides,
-              const Point *lower_base_center,
-              const Point *upper_base_center,
-              const GLdouble radius) {
-  Point points[4];
-  GLdouble angle, x, y, z;
-  int i;
-
-  drawInscribedPolygon(lower_base_center, radius, sides);
-  drawInscribedPolygon(upper_base_center, radius, sides);
-  for (i = 0; i <= sides; i++) {
-    angle = 2 * M_PI * i / sides;
-    points[0] = getPointOnCircumference(lower_base_center, radius, angle);
-    points[1] = getPointOnCircumference(upper_base_center, radius, angle);
-
-    angle = 2 * M_PI * (i + 1) / sides;
-    points[2] = getPointOnCircumference(upper_base_center, radius, angle);
-    points[3] = getPointOnCircumference(lower_base_center, radius, angle);
-
-    drawRectangle(points);
-  }
-}
-
-
-
-void draw2DClock(const Point *center, const double radius, const double secondsRotation, const double minutesRotation, const double hoursRotation, const double bg[3], const double fg[3]) {
-  const int sides = 360;
-
-  glColor3dv(bg);
-  drawInscribedPolygon(center, radius, sides);
-  
-  glColor3dv(fg);
-  for (int i = 12; i > 0 ; --i) {
-    double angle = 2*M_PI - i*hour*2*M_PI/360.0 + M_PI/2.0;
-    double x = (radius-0.1)*cos(angle);
-    double y = (radius-0.1)*sin(angle);
-    glRasterPos3d(x, y, 0.0);
-    
-    char number[3];
-    sprintf(number, "%d", i);
-    for (char *c = number; *c != '\0'; ++c) {
-      glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *c);
-    }
-  }
-  
-  glPushMatrix();
-    glRotated(secondsRotation, 0.0, 0.0, 1.0);
-    glBegin(GL_LINES);
-      glVertex3d(center->x, center->y, center->z);
-      glVertex3d(0.0, 0.45, 0.0);
-    glEnd();
-  glPopMatrix();
-
-  glPushMatrix();
-    glRotated(minutesRotation, 0.0, 0.0, 1.0);
-    glBegin(GL_LINES);
-      glVertex3d(center->x, center->y, center->z);
-      glVertex3d(0.0, 0.4, 0.0);
-    glEnd();
-  glPopMatrix();
-
-  glPushMatrix();
-    glRotated(hoursRotation, 0.0, 0.0, 1.0);
-    glBegin(GL_LINES);
-      glVertex3d(center->x, center->y, center->z);
-      glVertex3d(0.25, 0.0, 0.0);
-    glEnd();
-  glPopMatrix();
-}
-
-
-
-void draw3DClock(const Point *center, const double radius, const double secondsRotation, const double minutesRotation, const double hoursRotation, const double bg[3], const double fg[3]) {
-  Point backCenter = {center->x, center->y, center->z - 0.5};
-  draw2DClock(center, radius, secondsRotation, minutesRotation, hoursRotation, bg, fg);
-  draw2DClock(&backCenter, radius, secondsRotation, minutesRotation, hoursRotation, bg, fg);
-}
-
 
 
 void drawPolyedron(const Mesh *mesh){
-  Face *f;
+  Face f;
   for (int i = 0; i < mesh->numFaces; i++) {
-    f = &(mesh->faces[i]);
+    f = mesh->faces[i];
     glBegin(GL_POLYGON);
-      glNormal3d(f->normal.x, f->normal.y, f->normal.z);
-      for (int j = 0; j < f->numVerts; j++) {
-        glVertex3d(f->verts[j].x, f->verts[j].y, f->verts[j].z);
+      glNormal3d(f.normal[X], f.normal[Y], f.normal[Z]);
+      for (int j = 0; j < f.numVerts; j++) {
+        glVertex3d(f.verts[j][X], f.verts[j][Y], f.verts[j][Z]);
       }
     glEnd();
   }
 }
 
 
-
-Point newell(const Point *verts, const unsigned int numVerts) {
-    Point normal = {0.0, 0.0, 0.0};
-    for (unsigned int i = 0; i < numVerts; i++) {
-        const Point p = verts[i];
-        const Point next = verts[(i + 1) % numVerts];
-        normal.x += (p.y - next.y) * (p.z + next.z);
-        normal.y += (p.z - next.z) * (p.x + next.x);
-        normal.z += (p.x - next.x) * (p.y + next.y);
-    }
-    return normal;
-}
-
-
-
 void plotFunction(double (*f)(const double, const double, const double), const double domain[2][2], const double step, const double k) {
   for (double x = domain[0][0]; x <= domain[0][1]; x += step) {
     for (double z = domain[1][0]; z <= domain[1][1]; z += step) {
+        Point p[4];
+        p[0][X] = x;
+        p[0][Y] = f(x, z, k);
+        p[0][Z] = z;
+
+        p[1][X] = x;
+        p[1][Y] = f(x, z + step, k);
+        p[1][Z] = z + step;
+        
+        p[2][X] = x + step;
+        p[2][Y] = f(x + step, z + step, k);
+        p[2][Z] = z + step;
+        
+        p[3][X] = x + step;
+        p[3][Y] = f(x + step, z, k);
+        p[3][Z] = z;
+        
+        Point normal;
+        newell(normal, p, 4);
+        
         glBegin(GL_QUADS);
-          Point p0 = {x, f(x, z, k), z};
-          Point p1 = {x, f(x, z + step, k), z + step};
-          Point p2 = {x + step, f(x + step, z + step, k), z + step};
-          Point p3 = {x + step, f(x + step, z, k), z};
-          Point normal = newell((Point[]){p0, p1, p2, p3}, 4);      
-          glNormal3d(normal.x, normal.y, normal.z);
-          glVertex3d(p0.x, p0.y, p0.z);
-          glVertex3d(p1.x, p1.y, p1.z);
-          glVertex3d(p2.x, p2.y, p2.z);
-          glVertex3d(p3.x, p3.y, p3.z);
+          glNormal3d(normal[X], normal[Y], normal[Z]);
+          glVertex3d(p[0][X], p[0][Y], p[0][Z]);
+          glVertex3d(p[1][X], p[1][Y], p[1][Z]);
+          glVertex3d(p[2][X], p[2][Y], p[2][Z]);
+          glVertex3d(p[3][X], p[3][Y], p[3][Z]);
         glEnd();
       }
     }
 }
 
 
-
-Point casteljau(const Point *CP, const unsigned int N, const double t) {
-    Point *p = malloc(N * sizeof(Point));
-    for (int i = 0; i < N; i++) {
-        p[i].x = CP[i].x;
-        p[i].y = CP[i].y;
-        p[i].z = CP[i].z;
-    }
-    for (int r = 1; r < N; r++) {
-        for (int i = 0; i < N - r; i++) {
-            double x = (1-t)*p[i].x + t*p[i+1].x;
-            double y = (1-t)*p[i].y + t*p[i+1].y;
-            double z = (1-t)*p[i].z + t*p[i+1].z;
-            p[i].x = x;
-            p[i].y = y;
-            p[i].z = z;
-        }
-    }
-    Point result = p[0];
-    free(p);
-    return result;
-}
-
-
-
-void bezierCurve(const Point *CP, const unsigned int N, const double a, const double b) {
+void bezierCurve(const Point CP[], const unsigned int N, const double a, const double b) {
   glBegin(GL_LINE_STRIP);
-    glVertex3d(CP[0].x, CP[0].y, CP[0].z);
+    glVertex3d(CP[0][X], CP[0][Y], CP[0][Z]);
     for (double s = a; s <= b; s += 0.01) {
       const double t = (s - a) / (b - a);
-      Point p = casteljau(CP, N, t);
-      glVertex3d(p.x, p.y, p.z);
+      Point p;
+      casteljau(p, CP, N, t);
+      glVertex3d(p[X], p[Y], p[Z]);
     }
   glEnd();
 }
 
 
-
-int toHomogeneousCoordinates(double pw[4], const Point *p, const double w) {
-  if (pw == NULL || p == NULL) {
-    fprintf(stderr, "Error: pw and p cannot be NULL.\n");
-    return 1;
-  }
-  pw[0] = p->x * w;
-  pw[1] = p->y * w;
-  pw[2] = p->z * w;
-  pw[3] = w;
-  return 0;
-}
-
-
-
-int toHomogeneousCoordinatesv(double pw[4], const double p[3], const double w) {
-  if (pw == NULL || p == NULL) {
-    fprintf(stderr, "Error: pw and p cannot be NULL.\n");
-    return 1;
-  }
-  pw[X] = p[X] * w;
-  pw[Y] = p[Y] * w;
-  pw[Z] = p[Z] * w;
-  pw[3] = w;
-  return 0;
-}
-
-
-
-void rationalBezierCurve(const Point *CP, const double *w, const unsigned int N) {
+void rationalBezierCurve(const Point CP[], const double w[], const unsigned int N) {
   double *cpw = malloc(N * 4 * sizeof(double));
-
   for (int i = 0; i < N; i++) {
-    (void)toHomogeneousCoordinates(&cpw[i*4], &CP[i], w[i]);
+    toHomogeneousCoordinates(&cpw[i*4], CP[i], w[i]);
   }
-
   glMap1d(GL_MAP1_VERTEX_4, 0.0, 1.0, 4, N, cpw);
   glEnable(GL_MAP1_VERTEX_4);
-  
   glMapGrid1d(30, 0, 1);
   glEvalMesh1(GL_LINE, 0, 30);
-
   free(cpw);
 }
 
 
-
-int checkContinuity(const Point *CP1, const unsigned int N1,
-                    const Point *CP2, const unsigned int N2) {
-  if (CP1[N1-1].x != CP2[0].x || CP1[N1-1].y != CP2[0].y || CP1[N1-1].z != CP2[0].z) {
-    fprintf(stderr, "Error: The last control point of the first curve must be the same as the first control point of the second curve.\n");
-    return 0;
-  }
-  if (CP1[N1-1].x - CP1[N1-2].x != CP2[1].x - CP2[0].x || CP1[N1-1].y - CP1[N1-2].y != CP2[1].y - CP2[0].y || CP1[N1-1].z - CP1[N1-2].z != CP2[1].z - CP2[0].z) {
-    fprintf(stderr, "Warning: The tangent at the joining point is not the same for both curves.\n");
-    return 2;
-  }
-  return 1;
-}
-
-
-
-int compositeBezierCurve(const Point *CP1, const unsigned int N1, const double *w1,
-                          const Point *CP2, const unsigned int N2, const double *w2) {
-  if (!checkContinuity(CP1, N1, CP2, N2)) {
+int compositeBezierCurve(const Point CP1[], const unsigned int N1, const double w1[],
+                          const Point CP2[], const unsigned int N2, const double w2[]) {
+  if (!checkContinuity1(CP1, N1, CP2, N2)) {
     return 1;
   }
   rationalBezierCurve(CP1, w1, N1);
   rationalBezierCurve(CP2, w2, N2);
-  return 0;
-}
-
-
-
-void nurbsError(GLenum errorCode) {
-  const GLubyte *errorString = gluErrorString(errorCode);
-  fprintf(stderr, "Errore NURBS: %s\n", errorString);
-  exit(EXIT_FAILURE);
+  return GL_NO_ERROR;
 }
